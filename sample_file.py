ca17b9b0973a11ea0c93e5a9ed2c875d48888d5a
Ehsan Tork <journalehsan@gmail.com>
2024-04-07 09:54:31 +0330

    Create sample_file.py
    Sample file to show example usages of Persian/Jalali Date-Time.

---
+++ sample_file.py
@@ -0,0 +1,35 @@
+# Example:
+# date = PersianJalaliDate()
+# print(date.get_jalali_date())
+# print(date.get_jalali_date_time())
+# print(date.get_jalali_date_time_with_custom_output(custom_output="%s:%m:%H %d/%M/%y"))
+# print(date.convert_to_jalali_date(datetime.datetime.now()))
+# print(date.convert_to_jalali_date_time(datetime.datetime.now()))
+# print(date.convert_to_jalali_date_time_with_custom_output(datetime.datetime.now(), custom_output="%s:%m:%H %d/%M/%y"))
+# print(date.convert_to_jalali(datetime.datetime.now()))
+# print(date.convert_to_time(datetime.datetime.now()))
+# print(date.gregorian_to_jalali(2021, 10, 1))
+# print(date.g_days_in_month)
+# print(date.j_days_in_month)
+# print(date.gregorian_to_jalali(2021, 10, 1))
+def main():
+    date = PersianJalaliDate()
+    print(date.get_jalali_date())
+    print(date.get_jalali_date_time())
+    print(date.get_jalali_date_time_with_custom_output(custom_output="%s:%m:%H %d/%M/%y"))
+    print(date.convert_to_jalali_date(datetime.datetime.now()))
+    print(date.convert_to_jalali_date_time(datetime.datetime.now()))
+    print(
+        date.convert_to_jalali_date_time_with_custom_output(datetime.datetime.now(), custom_output="%s:%m:%H %d/%M/%y"))
+    print(date.convert_to_jalali(datetime.datetime.now()))
+    print(date.convert_to_time(datetime.datetime.now()))
+    print(date.gregorian_to_jalali(2021, 10, 1))
+    print(date.g_days_in_month)
+    print(date.j_days_in_month)
+    print(date.gregorian_to_jalali(2021, 10, 1))
+    # with day and month name
+    print(date.convert_to_jalali_with_day_month_name(datetime.datetime.now()))
+
+
+if __name__ == "__main__":
+    main()
